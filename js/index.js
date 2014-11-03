e2d14cf5429df691227bdb026202725643bb1482
nighca <nighca@live.cn>
2014-11-03 17:40:55 +0800

    upgrade x-client & no more X.ready nedded

--- js/index.js
+++ js/index.js
@@ -17,14 +17,6 @@ var inputs = {
     status: $('#status-in')
 };
 
-// init project & status list
-inputs.project.innerHTML = util.projectSequence.map(function(project){
-    return $.format('<option>${project}</option>', { project: project });
-}).join('');
-inputs.status.innerHTML = util.statusSequence.map(function(status){
-    return $.format('<option>${status}</option>', { status: status });
-}).join('');
-
 // validate inputs method
 var validate = function(){
     var legal = true;
@@ -39,78 +31,84 @@ var validate = function(){
     return legal;
 };
 
+// render list & bind events
+var renderList = function(tasks){
+    var data = util.transform(tasks);
+
+    // render list
+    list.innerHTML = template('template-list', $.extend(data, {
+        removeable: true
+    }));
+
+    // bind events (remove btn)
+    list.findAll('.j-remove').forEach(function(removeBtn){
+        var taskId = removeBtn.getAttribute('data-task-id');
+
+        removeBtn.on('click', function(e){
+            Task.remove({
+                _id: taskId
+            }, function(err, result){
+                if(err) util.handleError(err);
+                else refreshList();
+            });
+        });
+    });
+};
+
+// fetch task list & render list
+var refreshList = function(){
+    Task.list({
+        week: util.week,
+        person: inputs.person.value
+    }, function(err, tasks){
+        if(err) util.handleError(err);
+        else renderList(tasks);
+    });
+};
+
+// refresh list while person-input blurs
+inputs.person.on('blur', function(e){
+    // save to local
+    localStorage.username = inputs.person.value;
+
+    refreshList();
+});
+
+// do create task then refresh list while submit clicked
+$('#submit').on('click', function(e){
+    if(!validate()) return;
+
+    var task = {
+        person: inputs.person.value,
+        project: inputs.project.value,
+        cnt: inputs.task.value,
+        status: inputs.status.value,
+        week: util.week
+    };
+
+    Task.create(task, function(err, task){
+        if(err){
+            util.handleError(err);
+        }else{
+            inputs.task.value = '';
+            refreshList();
+        } 
+    });
+});
+
+// init project & status option list
+inputs.project.innerHTML = util.projectSequence.map(function(project){
+    return $.format('<option>${project}</option>', { project: project });
+}).join('');
+
+inputs.status.innerHTML = util.statusSequence.map(function(status){
+    return $.format('<option>${status}</option>', { status: status });
+}).join('');
+
 // restore username
 localStorage.username && (inputs.person.value = localStorage.username);
 
-X.ready(function(){
-
-    // render list & bind events
-    var renderList = function(tasks){
-        var data = util.transform(tasks);
-
-        // render list
-        list.innerHTML = template('template-list', $.extend(data, {
-            removeable: true
-        }));
-
-        // bind events (remove btn)
-        list.findAll('.j-remove').forEach(function(removeBtn){
-            var taskId = removeBtn.getAttribute('data-task-id');
-
-            removeBtn.on('click', function(e){
-                Task.remove({
-                    _id: taskId
-                }, function(err, result){
-                    if(err) util.handleError(err);
-                    else refreshList();
-                });
-            });
-        });
-    };
-
-    // fetch task list & render list
-    var refreshList = function(){
-        Task.list({
-            week: util.week,
-            person: inputs.person.value
-        }, function(err, tasks){
-            if(err) util.handleError(err);
-            else renderList(tasks);
-        });
-    };
-
-    // refresh list while person-input blurs
-    inputs.person.on('blur', function(e){
-        // save to local
-        localStorage.username = inputs.person.value;
-
-        refreshList();
-    });
-
-    // do create task then refresh list while submit clicked
-    $('#submit').on('click', function(e){
-        if(!validate()) return;
-
-        var task = {
-            person: inputs.person.value,
-            project: inputs.project.value,
-            cnt: inputs.task.value,
-            status: inputs.status.value,
-            week: util.week
-        };
-
-        Task.create(task, function(err, task){
-            if(err){
-                util.handleError(err);
-            }else{
-                inputs.task.value = '';
-                refreshList();
-            } 
-        });
-    });
-
-    // refresh list
-    refreshList();
-});
+// refresh list
+refreshList();
 
 })();
--- js/list.js
+++ js/list.js
@@ -6,56 +6,53 @@ util.setSideLink('返回', './index.html');
 // model Task
 var Task = util.Task;
 
-X.ready(function(){
+// doms
+var main = $('#main'),
+    personList = $('#person-list'),
+    list = $('#list'),
+    downLink = $('#down-link');
 
-    // doms
-    var main = $('#main'),
-        personList = $('#person-list'),
-        list = $('#list'),
-        downLink = $('#down-link');
+// render list method
+var renderList = function(tasks){
+    // do transform (task list -> data including projects)
+    var data = util.transform(tasks);
 
-    // render list method
-    var renderList = function(tasks){
-        // do transform (task list -> data including projects)
-        var data = util.transform(tasks);
-
-        // person list
-        var exists = {},
-            persons = tasks.map(function(task){
-            return task.person;
-        }).filter(function(person){
-            return exists[person] ? false : (exists[person] = true);
-        });
-        personList.innerHTML = '已提交：' + (persons.join('，') || '无');
-
-        // list
-        list.innerHTML = template('template-list', data);
-
-        // dowload link
-        var downContent = data.projects.map(function(project){
-            return $.format('${project}：\r\n\r\n\t${tasks}', {
-                project: project.name,
-                tasks: project.tasks.map(function(task, i){
-                    task.seq = i + 1;
-                    return $.format('${seq}. ${cnt} （${status}） 【${person}】', task);
-                }).join('\r\n\t')
-            });
-        }).join('\r\n\r\n');
-        downLink.download = $.format('周报${begin}-${end}.txt', $.map(util.weekRange, util.formatDate.bind(null, '${y}.${m}.${d}')));
-        downLink.href = URL.createObjectURL(new Blob([downContent]));
-    };
-
-    // get list and render
-    Task.list({
-        week: util.week
-    }, function(err, list){
-        if(err){
-            util.handleError(err);
-        }else{
-            renderList(list);
-            main.show();
-        }
+    // person list
+    var exists = {},
+        persons = tasks.map(function(task){
+        return task.person;
+    }).filter(function(person){
+        return exists[person] ? false : (exists[person] = true);
     });
+    personList.innerHTML = '已提交：' + (persons.join('，') || '无');
+
+    // list
+    list.innerHTML = template('template-list', data);
+
+    // dowload link
+    var downContent = data.projects.map(function(project){
+        return $.format('${project}：\r\n\r\n\t${tasks}', {
+            project: project.name,
+            tasks: project.tasks.map(function(task, i){
+                task.seq = i + 1;
+                return $.format('${seq}. ${cnt} （${status}） 【${person}】', task);
+            }).join('\r\n\t')
+        });
+    }).join('\r\n\r\n');
+    downLink.download = $.format('周报${begin}-${end}.txt', $.map(util.weekRange, util.formatDate.bind(null, '${y}.${m}.${d}')));
+    downLink.href = URL.createObjectURL(new Blob([downContent]));
+};
+
+// get list and render
+Task.list({
+    week: util.week
+}, function(err, list){
+    if(err){
+        util.handleError(err);
+    }else{
+        renderList(list);
+        main.show();
+    }
 });
 
 })();
