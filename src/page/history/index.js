0292be35877bf265fe7599d443e8c4da0cf72606
nighca <nighca@live.cn>
2015-11-22 15:50:25 +0800

    history page

--- src/page/history/index.js
+++ src/page/history/index.js
@@ -6,15 +6,38 @@
 import './index.less'
 
 import React from 'react'
+import { Task as TaskModel } from 'model'
+import HistoryList from 'widget/history/list'
 
-class History extends React.Component {
-  render() {
+export default React.createClass ({
+
+  getInitialState: function () {
+    return {
+      histories: []
+    };
+  },
+
+  refreshHistories: function () {
+    let me = this
+
+    TaskModel.distinct('week').then(function(weeks){
+      me.setState({
+        histories: weeks.sort((a, b) => (b - a)).map(week => ({ week }))
+      })
+    });
+  },
+
+  componentDidMount: function () {
+    TaskModel.on('change', this.refreshHistories)
+    this.refreshHistories()
+  },
+
+  render: function () {
     return (
       <div className='main'>
-      	<p>hello, history</p>
+        <HistoryList histories={this.state.histories} />
       </div>
     )
   }
-}
 
-export default History
+})
