aff89b1e60a78e94d0ad2b377180026c02b569b9
nighca <nighca@live.cn>
2015-11-22 15:51:16 +0800

    list page

--- src/page/list/index.js
+++ src/page/list/index.js
@@ -6,15 +6,52 @@
 import './index.less'
 
 import React from 'react'
+import weeker from 'mixin/weeker'
+import ProjectList from 'widget/project/list'
+import { Task as TaskModel } from 'model'
+import { tasksToProjects } from 'widget/util'
 
-class List extends React.Component {
-  render() {
+export default React.createClass ({
+
+  mixins: [weeker],
+
+  getInitialState: function () {
+    return {
+      tasks: []
+    };
+  },
+
+  refreshTasks: function (params) {
+    let me = this
+
+    params = Object.assign({
+      week: this.getWeek()
+    }, params)
+
+    TaskModel.list(params).then(function (list) {
+      me.setState({
+        tasks: list
+      })
+    })
+  },
+
+  handleWeekChange: function(week) {
+    this.refreshTasks({
+      week: week
+    })
+  },
+
+  componentDidMount: function () {
+    TaskModel.on('change', this.refreshTasks.bind(this, null))
+    this.refreshTasks()
+  },
+
+  render: function () {
     return (
       <div className='main'>
-      	<p>hello, list</p>
+        <ProjectList projects={tasksToProjects(this.state.tasks)} />
       </div>
     )
   }
-}
 
-export default List
+})
