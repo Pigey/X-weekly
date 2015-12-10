acd6756e2811ade10ad6ec80bd6529af2d4e12d1
nighca <nighca@live.cn>
2015-12-11 00:12:25 +0800

    model occupied

--- src/model/index.js
+++ src/model/index.js
@@ -5,20 +5,30 @@
 
 import Model from './model'
 
+// task
 export class Task extends Model {
   static get modelName() {
     return 'task'
   }
 }
 
+// existed project
 export class Project extends Model {
   static get modelName() {
     return 'project'
   }
 }
 
+// status for task
 export class Status extends Model {
   static get modelName() {
     return 'status'
+  }
+}
+
+// level of one's being occupied
+export class Occupied extends Model {
+  static get modelName() {
+    return 'occupied'
   }
 }
