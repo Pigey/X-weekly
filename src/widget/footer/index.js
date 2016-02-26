6463cb1db34c861669cff901932239f563326df4
nighca <nighca@live.cn>
2016-02-26 16:43:34 +0800

    make Footer Functional Stateless Component

--- src/widget/footer/index.js
+++ src/widget/footer/index.js
@@ -7,16 +7,12 @@ import './index.less'
 
 import React from 'react'
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer className='w-footer'>
-        <div className="inner-footer">
-          <p className="about">WEEKLY @ CSFE</p>
-        </div>
-      </footer>
-    )
-  }
+export default () => {
+  return (
+    <footer className='w-footer'>
+      <div className="inner-footer">
+        <p className="about">WEEKLY @ CSFE</p>
+      </div>
+    </footer>
+  )
 }
-
-export default Footer
